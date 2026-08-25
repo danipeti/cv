b171b6db15c6e563757983a7391804472a02acf4
Dani Péter <danipeti@gmail.com>
2026-08-25 17:41:28 +0200

    Fix CSS module names

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      generateScopedName: "[name]__[local]___[hash:base64:5]",
+    },
+  },
 });
